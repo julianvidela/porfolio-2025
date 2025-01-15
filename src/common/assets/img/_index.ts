ea0e60f0c56d576875a1b2144a0ad8623f851a9c
julianvidela <Julianvyd@gmail.com>
2025-01-15 12:39:48 -0300

    fix error with route img

--- src/common/assets/img/_index.ts
+++ src/common/assets/img/_index.ts
@@ -1,7 +1,7 @@
 import imgProfile from './imgProfile.jpg';
 import imgFlework from './imgFlexwork.png'
 import imgWhitelist from './imgWhitelist.png'
-import imgCoinLatam from './imgCoinLatam.png'
+import imgCoinLatam from './imgCoinlatam.png'
 
 
 const assets = { imgProfile: imgProfile.src, imgFlework: imgFlework.src, imgWhitelist: imgWhitelist.src, imgCoinLatam: imgCoinLatam.src };
